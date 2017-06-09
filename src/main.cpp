f67747111e32b246d352e417e44989cbf3fd01a4
Pedro H. B. Cavalcante <pedrinho.sjs@ufrn.edu.br>
2017-06-09 11:52:41 -0300

    Hello world

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 9 de jun de 2017
+ *      Author: SINFO
+ */
+
+//============================================================================
+// Name        : Testes.cpp
+// Author      : Pedro H B Cavalcante
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World !!! asdasdasd" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
+
+
